505f05b2575b5f32f990bfdece90e46ca22483a4
Damian Dziura <kontakt@Damiandziura.pl>
2018-08-12 14:57:52 +0200

    Add project files.

---
+++ TasksMenager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TasksMenager.MvcApplication" Language="C#" %>
---
+++ TasksMenager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TasksMenager.Startup))]
+namespace TasksMenager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
